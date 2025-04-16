2986db2839b1c6fbe98354896267aea3a41f38a9
drcabreram <drcabrera@cpp.edu>
2025-04-15 23:44:14 -0700

    Create Main.cpp

---
+++ Lab 5/Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Charmander.h"
+using namespace std;
+
+int main() {
+	vector<string> pokemonType;
+	pokemonType.push_back("Fire");
+
+	vector<string> skills;
+	skills.push_back("Growl");
+	skills.push_back("Scratch");
+
+	cout << "----------Constructor Created---------\n";
+	Charmander charlie = Charmander("Charlie", 100, 4, 4, pokemonType, skills);
+
+	cout << "----------Direct Speak---------\n";
+	charlie.speak();
+
+	Pokemon* p1 = &charlie;
+	cout << "----------Speak called from Pokemon Pointer---------\n";
+	(*p1).speak();
+
+	Charmander* c1 = &charlie;
+	cout << "----------Speak called from Charmander Pointer---------\n";
+	(*c1).speak();
+
+	cout << "----------Print Stats---------\n";
+
+	charlie.printStats();
+}
